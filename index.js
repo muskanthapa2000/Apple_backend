d4a6abcdcb3e816f1b20fd250eaebb856da734dc
muskanthapa2000 <7022muskan@gmail.com>
2023-09-20 14:13:39 +0530

    d

--- index.js
+++ index.js
@@ -15,50 +15,61 @@ app.get("/" , (req , res)=>{
     res.send("Home Page");
 })
 
-app.post("/signup" , async(req , res)=>{
-    const {email , password , name} = req.body;
-    const is_user = await UserModel.findOne({email , password , name})
-    if (is_user)
-    {
-        res.send("This email id is already registerd try another one")
-    }
 
-    bcrypt.hash(password, 3 , async function(err, hash) {
-        const new_user = new UserModel({
-            email : email, 
-            password : hash,
-            name : name
-        })
-        const data = await new_user.save();
-        res.send(data)
+
+app.post("/signup", async (req, res) => {
+    const { email, password, name } = req.body;
+    const isUser = await UserModel.findOne({ email });
+  
+    if (isUser) {
+      return res.send("This email id is already registered. Please use another one.");
+    }
+  
+    bcrypt.hash(password, 10, async function(err, hash) {
+      if (err) {
+        console.error("Error while hashing password:", err);
+        return res.status(500).send("Internal server error");
+      }
+  
+      const newUser = new UserModel({
+        email: email,
+        password: hash,
+        name: name,
+      });
+  
+      try {
+        const data = await newUser.save();
+        res.send(data);
+      } catch (error) {
+        console.error("Error while saving user:", error);
+        res.status(500).send("Internal server error");
+      }
     });
-})
+  });
+  
 
 
-app.post("/login" , async(req , res)=>{
-
-    const {email , password} = req.body;
-
-    const is_user = await UserModel.findOne({email})
-    if (is_user)
-    {
-        const hashed_pwd = is_user.password
-        bcrypt.compare(password, hashed_pwd , function(err, result) {
-            if (result)
-            {
-                var token = jwt.sign({ foo: 'bar' }, 'shhhhh');
-                 res.send({msg :"login successful" , token : token})
-            }
-            else {
-                res.send({msg : "Login first"})
-            }
-        }); 
+  app.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+  
+    const is_user = await UserModel.findOne({ email });
+  
+    if (is_user) {
+      const hashed_pwd = is_user.password;
+  
+      bcrypt.compare(password, hashed_pwd, function(err, result) {
+        if (result) {
+          var token = jwt.sign({ foo: 'bar' }, 'shhhhh');
+          res.send({ msg: "Login successful", token: token });
+        } else {
+          res.send({ msg: "Wrong password. Please try again." });
+        }
+      });
+    } else {
+      res.send("User not found. Please sign up first.");
     }
-    else {
-        res.send("signup first");
-    }
-    
-})
+  });
+  
 
 
 
